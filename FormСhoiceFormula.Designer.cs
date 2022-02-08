651ba2a282ca1b98ec5955b5fecba4fa93934086
Alexandr Roslyakov <99288384+KingDog25@users.noreply.github.com>
2022-02-09 02:30:03 +0300

    Add files via upload

---
+++ FormСhoiceFormula.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace _23_variant_CSharp_1_6
+{
+    partial class FormСhoiceFormula
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonСalculate = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 19);
+            this.label1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 83);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 47;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(790, 85);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.163636F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(279, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(225, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Вариант 23. Исходные данные";
+            // 
+            // buttonСalculate
+            // 
+            this.buttonСalculate.Location = new System.Drawing.Point(337, 12);
+            this.buttonСalculate.Name = "buttonСalculate";
+            this.buttonСalculate.Size = new System.Drawing.Size(121, 35);
+            this.buttonСalculate.TabIndex = 4;
+            this.buttonСalculate.Text = "Рассчитать";
+            this.buttonСalculate.UseVisualStyleBackColor = true;
+            this.buttonСalculate.Click += new System.EventHandler(this.buttonСalculate_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 204);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersWidth = 47;
+            this.dataGridView2.RowTemplate.Height = 28;
+            this.dataGridView2.Size = new System.Drawing.Size(790, 603);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.163636F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(312, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Таблица для расчета";
+            // 
+            // FormСhoiceFormula
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.buttonСalculate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Name = "FormСhoiceFormula";
+            this.Text = "Вычисление таблицы значений функции одного аргумента с выбором формулы";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonСalculate;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FormСhoiceFormula.cs
@@ -0,0 +1,91 @@
+﻿/*Вычисление таблицы значений функции одного аргумента с выбором формулы*/
+/*Составить программу вычисления N значений функции Y для X, изменяющегося от X1 с шагом dX.
+ Для проверки правильности программы задать значения для А, X1 и dX из второй таблицы*/
+
+/* Calculation of the table of values of the function of one argument with the choice of the formula */
+/*Compose a program to calculate N values of the function Y for X, which varies from X1 with a step of dX.
+  To check the correctness of the program, set the values \u200b\u200bfor A, X1 and dX from the second table */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _23_variant_CSharp_1_6
+{
+    public partial class FormСhoiceFormula : Form
+    {
+        public FormСhoiceFormula()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonСalculate_Click(object sender, EventArgs e)
+        {
+            int n = 23, a = 1;
+            double x1 = -2.2 * a;
+            double dX = (double)a/5;
+            string[] tableX = new string[n];
+            dataGridView1.RowCount = 1;                             //кол-во строк
+            dataGridView1.ColumnCount = 4;                          //столбцов
+            dataGridView1.Columns[0].HeaderText = "N";
+            dataGridView1.Columns[1].HeaderText = "Значение А";
+            dataGridView1.Columns[2].HeaderText = "Значение X1";
+            dataGridView1.Columns[3].HeaderText = "Шаг dX";
+
+            dataGridView2.RowCount = n;                             //кол-во строк
+            dataGridView2.ColumnCount = 5;                          //столбцов
+            dataGridView2.Columns[0].HeaderText = "N";
+            dataGridView2.Columns[1].HeaderText = "Значение А";
+            dataGridView2.Columns[2].HeaderText = "Значение X1";
+            dataGridView2.Columns[3].HeaderText = "Шаг dX";
+            dataGridView2.Columns[4].HeaderText = "Результат функции f(x)";
+
+            tableX[0] = "23";
+            tableX[1] = "1";
+            tableX[2] = "-2,2A";
+            tableX[3] = "A/5";
+            for (int j = 0; j < dataGridView1.ColumnCount; j++)                                         //заполняем значения X
+            {
+                dataGridView1.Rows[0].Cells[j].Value = tableX[j];
+            }
+
+            double[,] tableY = new double[n, dataGridView2.ColumnCount];
+            for (int i = 0; i < n; i++)
+            {
+                tableY[i, 0] = i+1;
+                dataGridView2.Rows[i].Cells[0].Value = (i+1).ToString("0.####");
+                
+                tableY[i, 1] = a;
+                dataGridView2.Rows[i].Cells[1].Value = a.ToString("0.####");
+
+                tableY[i, 2] = x1;
+                dataGridView2.Rows[i].Cells[2].Value = x1.ToString("0.####");
+
+                tableY[i, 3] = dX;
+                dataGridView2.Rows[i].Cells[3].Value = dX.ToString("0.####");
+
+                tableY[i, 4] = FunctRezult(x1, a);
+                dataGridView2.Rows[i].Cells[4].Value = FunctRezult(x1, a).ToString("0.####");
+
+                x1 += dX;                                                       //добавляем шаг
+             }
+
+            double FunctRezult(double x, double a)
+            {
+                double y;
+                if (x < 0)
+                    y = a + Math.Sqrt(2 * a * Math.Abs(x)) - Math.Abs(x);
+                else
+                    y = a * Math.Sqrt(3) - Math.Pow(x, 2) + Math.Pow(a, 2);
+                return y;
+            }
+
+        }
+    }
+}
